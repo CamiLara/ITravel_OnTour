c046ba4cd093f29d85b846284a30e0a26a7715f9
CamiLara <camila.palome@gmail.com>
2017-10-09 20:39:03 -0300

    se agrega template de administración

--- ITravel_OnTour/App_Start/BundleConfig.cs
+++ ITravel_OnTour/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace ITravel_OnTour
 {
@@ -8,24 +7,24 @@ namespace ITravel_OnTour
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/bundles/css").
+                Include(
+                "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                "~/Content/vendor/metisMenu/metisMenu.min.css",
+                "~/Content/dist/css/sb-admin-2.css",
+                "~/Content/vendor/morrisjs/morris.css",
+                "~/Content/vendor/font-awesome/css/font-awesome.min.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                   bundles.Add(new ScriptBundle("~/bundles/js").
+                Include(
+                "~/Content/vendor/jquery/jquery.min.js",
+                "~/Content/dist/js/sb-admin-2.js",
+                "~/Content/data/morris-data.js",
+                "~/Content/vendor/morrisjs/morris.min.js",
+                "~/Content/vendor/raphael/raphael.min.js",
+                "~/Content/vendor/metisMenu/metisMenu.min.js",
+                "~/Content/vendor/bootstrap/js/bootstrap.min.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
         }
     }
 }
