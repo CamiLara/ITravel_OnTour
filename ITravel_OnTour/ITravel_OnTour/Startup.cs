8718cd4a12db843110845ba1f550acce6b46626a
CamiLara <camila.palome@gmail.com>
2017-10-16 01:47:08 -0300

    autenticacion

---
+++ ITravel_OnTour/ITravel_OnTour/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITravel_OnTour.Startup))]
+namespace ITravel_OnTour
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
